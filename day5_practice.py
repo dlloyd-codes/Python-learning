cbc092e65bb294339fa95ec7e5dd78ca0bd2e8b6
Daniel Lloyd <danny.lloyd.2025@gmail.com>
2026-05-17 20:20:04 +0100

    day5

---
+++ day5_practice.py
@@ -0,0 +1,11 @@
+#Job interview scores
+student_scores = [78, 65, 89, 86, 55, 91, 64, 89]
+
+highest_score = 0
+
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+
+print(f"The highest socore in the class is {highest_score}")
+
