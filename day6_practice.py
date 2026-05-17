e54106e4c692bed0b3d352f2836dbeeba47f0bff
Daniel Lloyd <danny.lloyd.2025@gmail.com>
2026-05-17 20:15:38 +0100

    Functions

---
+++ day6_practice.py
@@ -0,0 +1,15 @@
+def greet_anyone(name):
+    print(f"Welcome back, {name}!")
+
+greet_anyone("Daniel")
+greet_anyone("Sarah")
+greet_anyone("Alex")
+
+
+def custom_countdown(start):
+    for number in range(1, start + 1):
+        print(number)
+
+custom_countdown(6)
+custom_countdown(8)
+
